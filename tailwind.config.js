e69c9de6aa061454a2e8d6fae8be7e9c8dab0a7c
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2023-10-10 09:57:19 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/components/**/*.(html|tsx)", "./src/**/*.(html|tsx)"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    prefix: "daisy",
+  },
+};
